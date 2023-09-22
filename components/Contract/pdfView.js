bb4da6b182f848e92adb3259be7ee23d02032647
Emma Udeji <gogreneinfo@gmail.com>
2023-09-22 06:05:34 +0100

    started afresh to build app with node 16 removed pdf components

--- components/Contract/pdfView.js
+++ components/Contract/pdfView.js
@@ -1,22 +1,23 @@
-import { useEffect, useRef, useState } from "react";
-import { Document, Page, pdfjs } from "react-pdf";
-import "react-pdf/dist/esm/Page/AnnotationLayer.css";
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+// import { useEffect, useRef, useState } from "react";
+// import { Document, Page, pdfjs } from "react-pdf";
+// import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+// pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 export default function PDFView({ url }) {
   return (
-    <Document file={url} renderMode={"svg"} className="pdf-document-view">
-      <Page
-        className="pdf_page_root"
-        pageNumber={1}
-        width={200}
-        height={230}
-        renderAnnotationLayer={false}
-        renderTextLayer={false}
-        onClick={() => {
-          window.open(url, "_blank");
-        }}
-      />
-    </Document>
+    // <Document file={url} renderMode={"svg"} className="pdf-document-view">
+    //   <Page
+    //     className="pdf_page_root"
+    //     pageNumber={1}
+    //     width={200}
+    //     height={230}
+    //     renderAnnotationLayer={false}
+    //     renderTextLayer={false}
+    //     onClick={() => {
+    //       window.open(url, "_blank");
+    //     }}
+    //   />
+    // </Document>
+    <></>
   );
 }

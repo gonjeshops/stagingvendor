1aa13525f64ad02569e6a29e56c9c3466966645c
emmaudeji@office <grogreneinfo@gmail.com>
2023-11-09 10:30:46 +0100

    uploading the main branch the new gonje vendor repo

---
+++ notes.js
@@ -0,0 +1,17 @@
+`
+
+backendapi.gonje.com/vendor/quotes/count
+backendapi.gonje.com/supplier/quotes/count
+backendapi.gonje.com/wishlist/add
+backendapi.gonje.com/my/wishlist
+/supplier/product/total
+/supplier/shop/total
+/transaction/total
+/wishlist/total
+/vendor/b2b/my/orders
+/vendor/b2b/my/total/orders
+/update/vendor/details
+/update/supplier/details
+
+
+`

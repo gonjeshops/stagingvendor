aa2588d7c6f25a00b737b978d7ba6dc54dcb07ee
Jumbo Daniel <jumbodaniel15@gmail.com>
2023-11-11 23:44:53 +0800

    Added the tables to the expenses

---
+++ components/Layout/Expenses/ExpenseTab.jsx
@@ -0,0 +1,150 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { RequestTable } from "./RequestTable";
+import { productColumns } from "./columns";
+import { ReceivedTable } from "./ReceivedTable";
+import { ReceivedColumns } from "./RecievedColumns";
+
+const ExpenseTabs = () => {
+  const dummyProducts = [
+    {
+      image: "product1.jpg",
+      productName: "Product 1",
+      group: "Group A",
+      productType: "Type X",
+      price: 50.0,
+      salePrice: 45.0,
+      quantity: 100,
+      inStock: 80,
+      status: "Active",
+      receivedQuoteAction: "Action 1",
+    },
+    {
+      image: "product2.jpg",
+      productName: "Product 2",
+      group: "Group B",
+      productType: "Type Y",
+      price: 75.0,
+      salePrice: 70.0,
+      quantity: 150,
+      inStock: 120,
+      status: "Inactive",
+      receivedQuoteAction: "Action 2",
+    },
+    {
+      image: "product3.jpg",
+      productName: "Product 3",
+      group: "Group C",
+      productType: "Type Z",
+      price: 100.0,
+      salePrice: 90.0,
+      quantity: 200,
+      inStock: 160,
+      status: "Active",
+      receivedQuoteAction: "Action 3",
+    },
+    {
+      image: "product4.jpg",
+      productName: "Product 4",
+      group: "Group A",
+      productType: "Type X",
+      price: 60.0,
+      salePrice: 55.0,
+      quantity: 120,
+      inStock: 100,
+      status: "Active",
+      receivedQuoteAction: "Action 4",
+    },
+    {
+      image: "product5.jpg",
+      productName: "Product 5",
+      group: "Group B",
+      productType: "Type Y",
+      price: 90.0,
+      salePrice: 80.0,
+      quantity: 180,
+      inStock: 140,
+      status: "Inactive",
+      receivedQuoteAction: "Action 5",
+    },
+  ];
+  const InvoicingData = [
+    {
+      transactionId: "trx001",
+      invoiceId: "inv001",
+      orderId: "order001",
+      purchaseName: "Product A",
+      amount: 100,
+      dueDate: "2023-11-15",
+      status: "Paid",
+      PaidOn: "2023-11-10",
+      paymentMethod: "Credit Card",
+    },
+    {
+      transactionId: "trx002",
+      invoiceId: "inv002",
+      orderId: "order002",
+      purchaseName: "Product B",
+      amount: 75,
+      dueDate: "2023-11-20",
+      status: "Sent",
+      PaidOn: "2023-11-18",
+      paymentMethod: "PayPal",
+    },
+    {
+      transactionId: "trx003",
+      invoiceId: "inv003",
+      orderId: "order003",
+      purchaseName: "Product C",
+      amount: 50,
+      dueDate: "2023-11-25",
+      status: "Overdue",
+      PaidOn: "2023-11-24",
+      paymentMethod: "Bank Transfer",
+    },
+    {
+      transactionId: "trx004",
+      invoiceId: "inv004",
+      orderId: "order004",
+      purchaseName: "Product D",
+      amount: 120,
+      dueDate: "2023-11-30",
+      status: "Sent",
+      PaidOn: "2023-11-27",
+      paymentMethod: "Cryptocurrency",
+    },
+    {
+      transactionId: "trx005",
+      invoiceId: "inv005",
+      orderId: "order005",
+      purchaseName: "Product E",
+      amount: 90,
+      dueDate: "2023-12-05",
+      status: "Paid",
+      PaidOn: "2023-12-02",
+      paymentMethod: "Debit Card",
+    },
+  ];
+
+  return (
+    <Tabs defaultValue="request" className="">
+      <TabsList className="grid max-w-lg grid-cols-2 bg-white rounded-md h-16 mx-auto">
+        <TabsTrigger value="request" className="py-2">
+          Request for Quote
+        </TabsTrigger>
+        <TabsTrigger value="receivedRequest" className="py-2">
+          Received Request
+        </TabsTrigger>
+      </TabsList>
+      <TabsContent value="request">
+        <RequestTable columns={productColumns} data={dummyProducts} />
+      </TabsContent>
+      <TabsContent value="receivedRequest">
+        <ReceivedTable columns={ReceivedColumns} data={InvoicingData} />
+      </TabsContent>
+    </Tabs>
+  );
+};
+export default ExpenseTabs;
--- pages/invoicing/edit/[invoiceId].jsx
+++ pages/invoicing/edit/[invoiceId].jsx
@@ -15,9 +15,6 @@ const Edit = () => {
         <div>
           <InvoiceGenerator />
         </div>
-        <div className="mt-4 flex justify-end">
-          <Button className="bg-gonje-green text-white">Update Invoice</Button>
-        </div>
       </section>
     </section>
   );
@@ -29,97 +26,51 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
 
-export const InputWithLabel = ({ name, label, type }) => {
+export const InputWithLabel = ({ name, label, type, onChange }) => {
   return (
     <div className="grid w-full max-w-sm items-center gap-1.5">
       <Label htmlFor="email" className="text-lg">
         {label}
       </Label>
-      <Input type={type} id="email" className={`h-12`} name={name} />
+      <Input
+        type={type}
+        id="email"
+        className={`h-12`}
+        name={name}
+        onChange={onChange}
+      />
     </div>
   );
 };
 
 const InvoiceGenerator = () => {
-  const [invoiceData, setInvoiceData] = useState({
-    item: "",
-    quantity: 0,
-    price: 0,
-    amount: 0,
-  });
-
-  const [invoice, setInvoice] = useState(null);
-
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setInvoiceData({ ...invoiceData, [name]: value });
-  };
-
-  const generateInvoice = () => {
-    // Calculate total including VAT
-    const total =
-      invoiceData.quantity * invoiceData.unitPrice * (1 + invoiceData.vatRate);
-
-    // Create the invoice object
-    const generatedInvoice = {
-      item: invoiceData.item,
-      quantity: invoiceData.quantity,
-      price: invoiceData.price,
-      amount: invoiceData.amount,
-      total: total.toFixed(2),
-    };
-
-    // Set the generated invoice
-    setInvoice(generatedInvoice);
-  };
-
   return (
     <div>
-      <div className="bg-gonje p-4 my-4 rounded-md text-lg font-semibold grid grid-cols-1 md:grid-cols-5 gap-x-6">
+      <div className="bg-gonje p-4 my-4 rounded-md text-lg font-semibold grid grid-cols-1 md:grid-cols-4 gap-x-6">
         <h2>Items</h2>
         <h2>Quantity</h2>
         <h2>Price</h2>
         <h2>Amount</h2>
-        <h2>Action</h2>
       </div>
-      <form className="grid grid-cols-1 md:grid-cols-5 gap-x-6">
+      <form className="grid grid-cols-1 md:grid-cols-4 gap-x-6">
         <div>
-          <Input
-            type={`text`}
-            name="item"
-            value={invoiceData.item}
-            onChange={handleInputChange}
-            className={`h-12`}
-          />
+          <span className={`w-full rounded-md px-3 py-1`}>Phone</span>
         </div>
-        <Input
-          type={`text`}
-          name="quantity"
-          value={invoiceData.quantity}
-          onChange={handleInputChange}
-          className={`h-12`}
-        />
-        <Input
-          type={`text`}
-          name="price"
-          value={invoiceData.price}
-          onChange={handleInputChange}
-          className={`h-12`}
-        />
-        <Input
-          type={`number`}
-          name="amount"
-          value={invoiceData.amount}
-          onChange={handleInputChange}
-          className={`h-12`}
-        />
-        <button onClick={generateInvoice}>Add</button>
+        <div>
+          <span className={`w-full rounded-md px-3 py-1`}>5</span>
+        </div>
+        <div>
+          <span className={`w-full rounded-md px-3 py-1`}>$500</span>
+        </div>
+        <div>
+          <span className={`w-full rounded-md px-3 py-1`}>$2500</span>
+        </div>
       </form>
 
       <div className="flex flex-col text-lg font-semibold space-y-4 divide-y-2  mt-5 text-right w-full">
-        <p>Subtotal: {invoice ? invoice.item ?? "" : ""}</p>
-        <p>Discount: {invoice ? invoice.quantity ?? "" : ""}</p>
-        <p>Total: {invoice ? invoice.total ?? "" : ""}</p>
+        <p>Subtotal:</p>
+        <p>Discount: </p>
+        <p>Total: </p>
       </div>
     </div>
   );
@@ -135,6 +86,31 @@ import {
 } from "@/components/ui/dialog";
 
 export const EmailDialog = () => {
+  const [users, setUsers] = useState([
+    { name: "John Doe", email: "johndoe@gmail.com" },
+  ]);
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+  });
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setUsers([
+      ...users,
+      {
+        name: formData.name,
+        email: formData.email,
+      },
+    ]);
+  };
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
   return (
     <Dialog>
       <DialogTrigger asChild>
@@ -152,34 +128,42 @@ export const EmailDialog = () => {
           <DialogTitle className="mb-4">Add Emails Address</DialogTitle>
         </DialogHeader>
         <div>
-          <div className="flex justify-around py-2 border text-lg">
+          <div className="flex justify-around py-3 border text-lg">
             <h3 className="">Name</h3>
             <h3 className="">Email</h3>
           </div>
-          <div className="border px-3 even:bg-slate-400 odd:bg-white">
-            <div className="flex justify-around py-2 text-lg">
-              <p> John Doe</p>
-              <p> johndoe@gmail.com</p>
-            </div>
-            <div className="flex justify-around py-2 text-lg">
-              <p> John Doe</p>
-              <p> johndoe@gmail.com</p>
-            </div>
-            <div className="flex justify-around py-2 text-lg" >
-            <p> John Doe</p>
-            <p> johndoe@gmail.com</p>
-          </div>   
+          <div className="border px-3 divide-y divide-slate-200 space-y-4">
+            {users.map((user, index) => (
+              <div key={index} className="flex justify-around py-2 text-lg">
+                <p>{user.name}</p>
+                <p>{user.email}</p>
+              </div>
+            ))}
           </div>
         </div>
-        <div className="grid grid-cols-2 gap-4 py-4">
-          <InputWithLabel label={`Name`} type={`text`} name={`name`} />
-          <InputWithLabel label={`Email`} type={`email`} name={`eamil`} />
-        </div>
-        <DialogFooter>
+        <form onSubmit={handleSubmit}>
+          <div className="grid grid-cols-2 gap-4 py-4">
+            <InputWithLabel
+              label={`Name`}
+              type={`text`}
+              name={`name`}
+              value={formData.name}
+              onChange={handleChange}
+            />
+            <InputWithLabel
+              label={`Email`}
+              type={`email`}
+              name={`email`}
+              value={formData.email}
+              onChange={handleChange}
+            />
+          </div>
           <Button type="submit" className="bg-gonje-green">
             Add Email
           </Button>
-        </DialogFooter>
+        </form>
+
+        <DialogFooter></DialogFooter>
       </DialogContent>
     </Dialog>
   );

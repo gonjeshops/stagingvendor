c70f6981969107e56d58432d39dc6b6a0684478d
emmaudeji@office <grogreneinfo@gmail.com>
2023-12-19 13:53:10 +0100

    updating notifications

--- components/Layout/Notifications/NotificationDropDown.jsx
+++ components/Layout/Notifications/NotificationDropDown.jsx
@@ -24,8 +24,6 @@ const NotificationDropdown = () => {
       console.log('Event pusher notification======', data.message);
       // setNotifications(prev=>[...prev, data.message])
       setPusherNotifications([data.message])
-      console.log('initial notification======', notifications);
-
     });
   }, [])
 
@@ -55,10 +53,10 @@ const NotificationDropdown = () => {
   }, [page]);
 
   return (
-    <div ref={modalRef} className="notification-modal z-50 w-full h-[300px] overflow-y-scroll">
+    <div ref={modalRef} className="notification-modal z-50 w-full h-[300px] overflow-y-scroll relative">
       <h1 className='pb-2 mb- border-b font-medium text-lg '>Notifications</h1>
       {error && <p>Error: {error.message}</p>}
-      <ul className='space-y-'>
+      <ul className='space-y '>
             {pusherNotifications?.map(({id, created_at, message, order_id, shop_id, status, title, updated_at, user_id}, index) => (
             <NotificationCard key={index} notifications={notifications} pusherNotifications={pusherNotifications} setNotifications={setNotifications} id={id} created_at={created_at} message={message} order_id={order_id} shop_id={shop_id} status={status} title={title} updated_at={updated_at} user_id={user_id}/>
               
@@ -66,8 +64,8 @@ const NotificationDropdown = () => {
             {notifications?.map(({id, created_at, message, order_id, shop_id, status, title, updated_at, user_id}, index) => (
             <NotificationCard key={index} notifications={notifications} pusherNotifications={pusherNotifications} setNotifications={setNotifications} id={id} created_at={created_at} message={message} order_id={order_id} shop_id={shop_id} status={status} title={title} updated_at={updated_at} user_id={user_id}/>
         ))}
+          {isLoading &&<div className="flex absolute inset-0 justify-center items-center"><BtnSpinner color={'green'}/></div>}
       </ul>
-      {/* {isLoading && <BtnSpinner/>} */}
     </div>
   );
 };

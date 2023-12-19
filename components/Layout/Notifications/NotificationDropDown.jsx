908fb9fd72776b00af78b2bf70956db6f4362a96
emmaudeji@office <grogreneinfo@gmail.com>
2023-12-19 13:47:07 +0100

    updating notifications

--- components/Layout/Notifications/NotificationDropDown.jsx
+++ components/Layout/Notifications/NotificationDropDown.jsx
@@ -3,6 +3,7 @@ import NotificationCard from './NotificationCard';
 import { BtnSpinner } from '@/componentsB2b/Loader/Spinner/BtnSpinner';
 import { fetchb2cNotificications } from '@/componentsB2b/Api2';
 import { vendorShopChannel } from '@/configs/pusherConfig';
+import { useGlobalState } from '@/context/GlobalStateContext';
 
 const NotificationDropdown = () => {
   const [notifications, setNotifications] = useState([]);
@@ -12,15 +13,19 @@ const NotificationDropdown = () => {
   const [page, setPage] = useState(1);
   let [totalPages, setTotalpages] = useState(2);
 
+  const {user}=useGlobalState()
+
 
   const modalRef = useRef();
 
   useEffect(() => {
-    const channel = vendorShopChannel('5')
+    const channel = vendorShopChannel(user?.shop_id)
     channel.bind('eventType', function (data) {
       console.log('Event pusher notification======', data.message);
-      setNotifications([...notifications, data.message])
+      // setNotifications(prev=>[...prev, data.message])
       setPusherNotifications([data.message])
+      console.log('initial notification======', notifications);
+
     });
   }, [])
 
@@ -28,22 +33,15 @@ const NotificationDropdown = () => {
     const fetchData = async () => {
       setIsLoading(true);
       try {
-        const response = await fetchb2cNotificications(page, 20, );
-  console.log('notifica====', response)
+        const response = await fetchb2cNotificications(page, 50, );
+        console.log('notifica====', response)
         if (response?.status === 200) {
           const data = response?.data?.data;
-          // Ensure that data.total_pages exists before updating total pages
           if (data.total_pages !== undefined) {
             setTotalpages(data.total_pages);
           }
-        //   setNotifications(data.notifications);
-          const newNotifications = data.notifications;
-          setNotifications((prevNotifications) => [
-            ...prevNotifications,
-            ...newNotifications,
-          ]);
+          setNotifications(data?.notifications);
           setIsLoading(false);
-          console.log('NOTIFICATION RESPONSE==== ', data);
         } else {
           console.log('NOTIFICATION ERROR RESPONSE==== ', response);
         }
@@ -56,50 +54,20 @@ const NotificationDropdown = () => {
     fetchData();
   }, [page]);
 
-  
-  
-  const handleScroll = () => {
-    const modal = modalRef.current;
-    if (modal) {
-        if (modal.scrollTop === 0) {
-            // User scrolled to the top, increase the page if not on the first page
-            if (page > 1) {
-              setPage((prevPage) => prevPage - 1);
-            }
-          } else if (modal.scrollTop + modal.clientHeight >= modal.scrollHeight - 10) {
-            // User scrolled to the bottom, increase the page if not on the last page
-            if (page < 4) {
-              setPage((prevPage) => prevPage + 1);
-            }
-          }
-          
-    }
-  };
-
-
-  useEffect(() => {
-    modalRef.current.addEventListener('scroll', handleScroll);
-    return () => {
-      modalRef.current?.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
-
   return (
-    <div ref={modalRef} className="notification-modal w-full h-[400px] overflow-y-scroll">
+    <div ref={modalRef} className="notification-modal z-50 w-full h-[300px] overflow-y-scroll">
       <h1 className='pb-2 mb- border-b font-medium text-lg '>Notifications</h1>
       {error && <p>Error: {error.message}</p>}
       <ul className='space-y-'>
             {pusherNotifications?.map(({id, created_at, message, order_id, shop_id, status, title, updated_at, user_id}, index) => (
-            //   <li className='pb-3' key={index}>{notification?.message}</li>
             <NotificationCard key={index} notifications={notifications} pusherNotifications={pusherNotifications} setNotifications={setNotifications} id={id} created_at={created_at} message={message} order_id={order_id} shop_id={shop_id} status={status} title={title} updated_at={updated_at} user_id={user_id}/>
               
             ))}
             {notifications?.map(({id, created_at, message, order_id, shop_id, status, title, updated_at, user_id}, index) => (
-            //   <li className='pb-3' key={index}>{notification?.message}</li>
             <NotificationCard key={index} notifications={notifications} pusherNotifications={pusherNotifications} setNotifications={setNotifications} id={id} created_at={created_at} message={message} order_id={order_id} shop_id={shop_id} status={status} title={title} updated_at={updated_at} user_id={user_id}/>
         ))}
       </ul>
-      {isLoading && <BtnSpinner/>}
+      {/* {isLoading && <BtnSpinner/>} */}
     </div>
   );
 };
--- configs/pusherConfig.js
+++ configs/pusherConfig.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import Pusher from 'pusher-js';
 
 export const pusher = new Pusher(process.env.NEXT_PUBLIC_PUSHER_APP_KEY, {
@@ -7,6 +8,33 @@ export const pusher = new Pusher(process.env.NEXT_PUBLIC_PUSHER_APP_KEY, {
 // connect to vendor's shop channel. if this is customer, pass the shopId from the product or shop data,
 export const vendorShopChannel = (shopId) => pusher.subscribe(String(shopId));
 
+export const triggerNotification = async (shopId, data) => {
+  try {
+    console.log('checking data', data)
+    const res = await axios.post('/api/notifications', 
+    {shopId: '5', 
+    eventType: 'eventType', 
+    message: "Shopping from me now.", 
+    data: {
+      "id": shopId,
+      "shop_id": data?.shop_id,
+      "shop_owner_id": data?.owner_id || data?.user_id || '',
+      "sender_id": data?.userId,
+      "order_id": null,
+      "title": data?.title,
+      "message": data?.message,
+      "module": "B2C",
+      "status": data?.status,
+      "read_at": null,
+      "created_at":  new Date().toLocaleString(),
+      "updated_at": new Date().toLocaleString()
+        }, 
+      }
+    )
+  } catch (error) {
+    console.log('pusher catch error: ', error)
+  }
+}
 // sample data to connect to a vendor shop's channel
 // useEffect(() => {
 //   const channel = vendorShopChannel(String(shopId))
@@ -26,3 +54,18 @@ export const vendorShopChannel = (shopId) => pusher.subscribe(String(shopId));
 // }
 
 // eventTypes = "product_purchase", "accept_order", "assign_delivery"
+
+// notification sample data
+// {
+//   "id": 1351,
+//   "shop_id": 5,
+//   "user_id": 8,
+//   "order_id": null,
+//   "title": "Quote Request",
+//   "message": "Quote request updated successfully",
+//   "module": "B2C",
+//   "status": 0,
+//   "read_at": null,
+//   "created_at": "2023-12-14 12:01:57",
+//   "updated_at": "2023-12-14 12:01:57"
+// }

07538e65bb310348a0499f61ca35d9ffa7ec6a45
emmaudeji@office <grogreneinfo@gmail.com>
2023-12-29 20:06:56 +0100

    google-places

--- pages/_document.js
+++ pages/_document.js
@@ -22,6 +22,10 @@ export default class CustomDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap"
             rel="stylesheet"
           />
+          <script
+            type="text/javascript"
+            src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_API_KEY}&libraries=places`}
+          />
         </Head>
         <body className="v-dashboard">
           <Main />
